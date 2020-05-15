eb2ceac2cb3665d21a32eb70fdd817e5ee88482b
baobao <923065971@qq.com>
2020-05-15 15:49:51 +0800

    setting

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,17 @@
-const { override, fixBabelImports } = require('customize-cra')
+const { override, fixBabelImports, addLessLoader } = require('customize-cra')
+// const CompressionWebpackPlugin = require('compression-webpack-plugin')
 
 module.exports = override(
   fixBabelImports('antd', {
     libraryDirectory: 'es',
-    style: 'css'
+    style: true
+  }),
+  addLessLoader({
+    lessOptions: {
+      javascriptEnabled: true,
+      modifyVars: { '@primary-color': '#5c7168' }
+    }
   })
 )
+
+
