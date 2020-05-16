0edee973e53ed61bf743eb92e22a929c77e49ee6
baozai <923065971@qq.com>
2020-05-16 23:34:12 +0800

    origin

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { Button } from 'antd'
+// import logo from './logo.svg';
+import { Button } from 'antd';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Button type="primary">Button</Button>
-      </header>
+      <Button type="primary">Button</Button>
     </div>
   );
 }
